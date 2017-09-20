04557dfefc95bfc67e01a1a8dc5e75245596c438
Julia Pulova <juliapulova@yandex.ru>
2017-09-20 11:42:25 +0300

    The first Servlet-Filter example
    Servlet-Filter is a class that implements  javax.servlet.Filter
    interface.

---
+++ src/com/bdgolka/tutorial/servletfilter/LogFilter.java
@@ -0,0 +1,46 @@
+package com.bdgolka.tutorial.servletfilter;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+
+public class LogFilter implements Filter {
+
+	public LogFilter() {
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		System.out.println("LogFilter init!");
+	}
+
+	@Override
+	public void destroy() {
+		System.out.println("LogFilter destroy!");
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		
+		String servletPath = req.getServletPath();
+		
+		System.out.println("#INFO" + new Date() + " - Sevlet Path :" + servletPath //
+				+ ", URL= " + req.getRequestURL());
+
+		//Go to the next element(filter or target) in chain
+		chain.doFilter(request, response);
+	}
+
+}
